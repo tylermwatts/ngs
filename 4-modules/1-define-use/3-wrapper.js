67559079a5d9db1ccab5000b97db12f4cfd8372d
Warpfox <tylermwatts@gmail.com>
2019-03-19 14:05:58 -0500

    wip

--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,1 +1,15 @@
-console.log(arguments);
+// Node wraps modules into functions like so:
+
+// function (exports, module, require, __filename, __dirname){
+
+// console.log(arguments);
+
+exports.a = 42;
+module.exports.b = 37;
+
+// exports = () => {}; will not work, breaks assignment reference
+
+module.exports = () => {}; // this does work because module.exports is what is being returned
+
+//  return module.exports;
+// } (module.exports)
